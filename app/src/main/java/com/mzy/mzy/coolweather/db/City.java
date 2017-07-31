8cbdbf89077ae2ae052737f1a5996307e93731d3
miaozhenyu <2982966869@qq.com>
2017-07-31 11:56:10 +0800

    完成遍历省市县三级列表的功能。

--- app/src/main/java/com/mzy/mzy/coolweather/db/City.java
+++ app/src/main/java/com/mzy/mzy/coolweather/db/City.java
@@ -10,7 +10,7 @@ public class City extends DataSupport {
     private int id;
     private String cityName;
     private int cityCode;
-    private int provincdId;
+    private int provinceId;
 
     public int getId() {
         return id;
@@ -36,11 +36,11 @@ public class City extends DataSupport {
         this.cityCode = cityCode;
     }
 
-    public int getProvincdId() {
-        return provincdId;
+    public int getProvinceId() {
+        return provinceId;
     }
 
-    public void setProvincdId(int provincdId) {
-        this.provincdId = provincdId;
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
     }
 }
