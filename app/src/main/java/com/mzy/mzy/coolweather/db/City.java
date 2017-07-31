afac2beb47b66e013971b022b8939f68d9fc4d89
miaozhenyu <2982966869@qq.com>
2017-07-31 09:04:51 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/mzy/mzy/coolweather/db/City.java
@@ -0,0 +1,46 @@
+package com.mzy.mzy.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by MZY on 2017/7/31.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provincdId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvincdId() {
+        return provincdId;
+    }
+
+    public void setProvincdId(int provincdId) {
+        this.provincdId = provincdId;
+    }
+}
